a49673aad1651d4f28fcbc5320d15445ca6bd167
LifeIsOne <didtmdgh258@gmail.com>
2024-01-30 21:06:17 +0900

    UserController 완성

--- src/main/java/shop/mtcoding/blog/user/UserController.java
+++ src/main/java/shop/mtcoding/blog/user/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
         //  2. Model에 위임하기
         userRepository.saveV2(requestDTO);
 
-        return "redirect:/loginFrom";
+        return "redirect:/loginForm";
     }
 
     // 로그인 Mapping

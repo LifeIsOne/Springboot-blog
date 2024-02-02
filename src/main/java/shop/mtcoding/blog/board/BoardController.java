916a7df54e61f13789ac60eb5f8bc6d6d8d82975
LifeIsOne <didtmdgh258@gmail.com>
2024-02-02 17:36:28 +0900

    Query로 받기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,18 +1,24 @@
-package shop.mtcoding.blog.controller;
+package shop.mtcoding.blog.board;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
 
+@RequiredArgsConstructor
 @Controller
 public class BoardController {
 
     private final HttpSession session;
+    private final BoardRepository boardRepository;
 
-    public BoardController(HttpSession session) {
-        this.session = session;
+    //  데이터 리턴시 (RestController or ResponseBody) - 객체를 리턴하면 스프링이 json으로 변환
+    @GetMapping("/api/board/{id}")
+    public @ResponseBody Board apiBoard(){
+        return boardRepository.findById(1);
     }
 
     @GetMapping({ "/", "/board" })
--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,2 +1,7 @@
-INSERT INTO user_tb(username, password, email, created_at) VALUES('matthew', '1234', 'matthew@123.456', now());
-INSERT INTO user_tb(username, password, email, created_at) VALUES('cos', '1234', 'cos@nate.com', now());
+INSERT INTO user_tb(username, password, email, created_at) VALUES('Kenneth', '1234', 'Minsu@Kim', now());
+INSERT INTO user_tb(username, password, email, created_at) VALUES('Denny', '1234', 'Youngju@Lee', now());
+
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목1', '내용1', 1, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목2', '내용2', 1, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목3', '내용3', 1, now());
+INSERT INTO board_tb(title, content, user_id, created_at) VALUES ('제목4', '내용4', 2, now());

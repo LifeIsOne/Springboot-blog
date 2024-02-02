090f4bc9bcd9f7bf0556bb773bb6f413e0efb2f4
LifeIsOne <didtmdgh258@gmail.com>
2024-02-02 17:49:16 +0900

    Query로 받기

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -1,9 +1,11 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
@@ -18,6 +20,7 @@ public class BoardController {
     //  데이터 리턴시 (RestController or ResponseBody) - 객체를 리턴하면 스프링이 json으로 변환
     @GetMapping("/api/board/{id}")
     public @ResponseBody Board apiBoard(){
+
         return boardRepository.findById(1);
     }
 
@@ -36,6 +39,12 @@ public class BoardController {
     @GetMapping("/board/saveForm")
     public String saveForm() {
         return "board/saveForm";
+    }
+
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable int id, HttpServletRequest request){
+        Board board = boardRepository.findById(id);
+        request.
     }
 
     @GetMapping("/board/1")
